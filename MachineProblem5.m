d04b4346893eaa8dedf41d84600d2c87940c6a58
eulanmanginsay <57347025+eulanmanginsay@users.noreply.github.com>
2019-12-08 19:20:00 +0800

    Add files via upload

---
+++ MachineProblem5.m
@@ -0,0 +1,21 @@
+n = 0:199;
+y = 0:199;
+x = input('Input equation of x(n): ');
+
+for a = 1:200
+    if y(a) == 0
+        y(a) = -1.5*x(a) + 2*x(a + 1) - 0.5*x(a + 2);
+    elseif y(a) > 0 && y(a) <= 198
+        y(a) = 0.5*x(a + 1) - 0.5*x(a - 1);
+    elseif y(a) == 199
+        y(a) = 1.5*x(a) - 2*x(a - 1) + 0.5*x(a - 2);
+    end
+end
+
+plot(n, x)
+hold on
+plot(n, y)
+legend('x(n)', 'y(n)')
+xlabel('n')
+ylabel('x, y')
+hold off
